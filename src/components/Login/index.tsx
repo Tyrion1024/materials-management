d1eba1549fd8fb5e6fcfad3934b21cf42c199e3c
zhiweiLi <zhiwei.li@fugetech.com>
2020-08-20 10:10:50 +0800

    update login

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import { connect } from 'react-redux'
 import { withRouter, RouteComponentProps } from 'react-router-dom'
-import { Input as AntdInput, Button as AntdButton, message as AntdMessage } from 'antd'
+import { Input as AntdInput, message as AntdMessage } from 'antd'
 import { setUserInfo } from '../../store/actions/user'
 import imgUrl from './assets/login_title.png'
 import Loading from '../../common/Loading'
@@ -13,9 +13,6 @@ interface LoginState {
 	account: string
 	password: string
 	loading: boolean
-	isDisabled: boolean
-  buttonName: string
-  interval: any
 }
 
 interface IProps {
@@ -34,18 +31,9 @@ class Login extends React.Component<propsType> {
     this.state = {
       account: '',
       password: '',
-      loading: false,
-			isDisabled: false,
-      buttonName: 'getCode',
-      interval: null
+      loading: false
     }
 	}
-
-  componentWillUnmount () {
-    if (this.state.interval) {
-      clearInterval(this.state.interval)
-    }
-  }
 
 	handleAccountChange = (e: any) => {
 		this.setState({
@@ -71,53 +59,15 @@ class Login extends React.Component<propsType> {
       AntdMessage.error('请输入手机号')
       return false
     }
+    if (!validatePhone(this.state.account)) {
+      AntdMessage.error('请输入正确的手机号')
+      return false
+    }
     if (this.state.password === '') {
-      AntdMessage.error('请输入验证码')
+      AntdMessage.error('请输入密码')
       return false
     }
     return true
-  }
-
-  getCode = () => {
-    if (!validatePhone(this.state.account)) {
-      AntdMessage.error('Please enter correct phone number')
-      return
-		}
-		this.setState({
-			isDisabled: true
-		})
-    axios.get('/sendVerificationCode', {
-      params: {
-        mobile_phone: this.state.account
-      }
-    })
-      .then((res: any) => {
-        if(res && res.code === 10100) {
-					this.setState({
-						isDisabled: false
-					})
-          return
-        }
-        let time = 59
-        let interval = setInterval(() => {
-          this.setState({
-						buttonName: '（' + time + 's) Resend'
-					})
-          time--
-          if (time < 0) {
-						this.setState({
-							buttonName: 'Resend',
-              isDisabled: false,
-              interval: null
-						})
-            time = 59
-            clearInterval(interval)
-          }
-        }, 1000)
-        this.setState({
-          interval: interval
-        })
-      })
   }
 
   login = () => {
@@ -160,9 +110,6 @@ class Login extends React.Component<propsType> {
   }
 
   render = () => {
-		const GetCodeBtn = () => {
-			return <AntdButton disabled={ this.state.isDisabled } type="link" onClick={ this.getCode }> { this.state.buttonName } </AntdButton>
-		}
     return (
       <div className="login">
 				{
@@ -179,7 +126,7 @@ class Login extends React.Component<propsType> {
 								<AntdInput onPressEnter={ this.onKeydown } value={ this.state.account } size="large" addonBefore="+86" placeholder="Enter Phone Number" onChange={ this.handleAccountChange }/>
 							</div>
 							<div className="login__password">
-								<AntdInput onPressEnter={ this.onKeydown } value={ this.state.password } size="large" placeholder="Enter Verification Code" addonAfter={ <GetCodeBtn /> } onChange={ this.handlePasswordChange } />
+								<AntdInput onPressEnter={ this.onKeydown } value={ this.state.password } size="large" placeholder="Enter Verification Code" onChange={ this.handlePasswordChange } />
 							</div>
 							<div className="login__button" onClick={ this.login }> Login </div>
 						</div>
